cee71a6e0dfa7b4d3d6220532b0ef65ebbc2eb57
LuizHMAL <leite.luizhenrique94@gmail.com>
2024-04-22 22:06:54 -0300

    main update

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,48 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
-
-export function Post(props) {
-    return (
-      <article className = {styles.Post}>
-        <header>
-          <div className={styles.author}>
-
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/luizhmal.png" />
+          <div className={styles.authorInfo}>
+            <strong>Luiz Henrique</strong>
+            <span>Intern</span>
           </div>
-        </header>
-      </article>
-    )
-  }
-  
+        </div>
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Este é um projeto para meu portfólio, ele está presente nas aulas da Rocketseat</p>
+        <p><a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+    
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+
+    </article>
+  )
+}
